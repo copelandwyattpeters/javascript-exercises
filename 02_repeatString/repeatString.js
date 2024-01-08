97de6a480d17ad80d9ede99fdaf452b8bb06c110
copelandwyattpeters <wyatt.peters@utexas.edu>
2024-01-08 10:45:20 -0600

    Completed Exercises 2 and 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    var word2 = ""
+    if (times < 0){
+        return "ERROR"
+    }
+    for (let i = 0; i < times; i++) {
+        word2 += word;
+    }
+    return word2
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    var word2 = ""
+    if (word.length >0){
+        for (let i = 0; i <= word.length-1; i++) {
+            word2 = word[i] + word2
+            
+        }
+    }
+    return word2
 };
 
 // Do not edit below this line
