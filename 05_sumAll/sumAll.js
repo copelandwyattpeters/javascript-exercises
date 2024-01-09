b421dcbd951b40b810ee007c7ae8bf7e9c3a07ff
copelandwyattpeters <wyatt.peters@utexas.edu>
2024-01-08 20:49:54 -0600

    Completed exercises 4-6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    var sum = 0;
+    var min = num1;
+    var max = num2;
+    if (num1 >= num2){
+        min = num2;
+        max = num1;
+    }
+    if (typeof num1 != "number"||typeof num2 != "number"){
+        return "ERROR";
+    }
+    if (num1>=0 && num2>=0){
+        for (let x = min; x<=max; x++){
+            sum += x;
+        }
+    }
+    else{
+        return "ERROR";
+    };
+    return sum;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function(num1) {
+    if (typeof num1 != "number"){
+        return "ERROR";
+    }
+    if (num1%4 ==0){
+        if (num1%100==0){
+            if (num1 % 400 ==0){
+                return true
+            }
+            else{
+                return false
+            }
+        }
+        return true
+    }
+    else{
+        return false
+    }
 };
 
 // Do not edit below this line
